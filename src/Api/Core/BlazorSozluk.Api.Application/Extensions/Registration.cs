7472b3ee0f5d400964cff0aedd1b4968d9f8e059
BerkayTuluk <96583955+BerkayTuluk@users.noreply.github.com>
2023-10-07 13:27:06 +0300

    LoginUser

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Extensions/Registration.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Extensions;
+
+public static class Registration
+{
+    public static IServiceCollection AddApplicationRegistration(this IServiceCollection services)
+    {
+        //Çalışan assemblylerini bulma (çalışan class librariyleride içerisinde barındırı)
+        var assm = Assembly.GetExecutingAssembly();
+
+
+        //bunun altında çalışan dienelleri tarıyarak ugun olan handlelirları kendisine ekleyecek
+        services.AddMediatR(assm);
+        services.AddAutoMapper(assm);
+        services.AddValidatorsFromAssembly(assm);
+
+        return services;
+    }
+}
